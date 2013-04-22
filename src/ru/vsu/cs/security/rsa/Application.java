c11514412f18f81da6b16304b40512451450e4bc
pshchegolevatykh <pshchegolevatykh@gmail.com>
2013-04-22 15:21:37 +0000

    Changed sample data to be less than key length.

--- src/ru/vsu/cs/security/rsa/Application.java
+++ src/ru/vsu/cs/security/rsa/Application.java
@@ -6,7 +6,7 @@ public class Application {
     public static void main(String[] args) {
         BigInteger e = BigInteger.valueOf(12371);
         BigInteger n = new BigInteger("517815623413379");
-        BigInteger cipher = new BigInteger("12788138155374656626484111508435839351545312207685619856968320863251613810");
+        BigInteger cipher = new BigInteger("127881381553746");
         RsaCracker cracker = new RsaCracker(e, n , cipher);
         try {
             BigInteger crackedMessage = cracker.crack();
